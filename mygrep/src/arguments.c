b0a81786b242b8c39126cfcd6ea2314de5e4a211
LetsMelon <domi.m@outlook.com>
2023-10-15 10:57:53 +0200

    move struct 'argument' logic into seperate file

---
+++ mygrep/src/arguments.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "arguments.h"
+
+void arguments_init(arguments_t *arg)
+{
+    arg->output_file = NULL;
+
+    arg->keyword = NULL;
+
+    arg->case_sensitive = true;
+
+    arg->input_files = NULL;
+    arg->input_files_num = 0;
+    arg->input_files_capacity = 0;
+}
+
+void arguments_free(arguments_t *arg)
+{
+    free(arg->output_file);
+    free(arg->keyword);
+
+    for (int i = 0; i < arg->input_files_num; i += 1)
+    {
+        free(arg->input_files[i]);
+    }
+    free(arg->input_files);
+    arg->input_files_num = 0;
+    arg->input_files_capacity = 0;
+}
+
+void arguments_add_input_file(arguments_t *arg, char *file)
+{
+    if (arg->input_files == NULL || arg->input_files_capacity == 0)
+    {
+        arg->input_files_capacity = 8;
+        arg->input_files = (char **)malloc(arg->input_files_capacity * sizeof(char *));
+        arg->input_files_num = 0;
+    }
+
+    if (arg->input_files_num >= arg->input_files_capacity)
+    {
+        arg->input_files_capacity = arg->input_files_capacity * 3 / 2;
+        arg->input_files = (char **)realloc(arg->input_files, arg->input_files_capacity * sizeof(char *));
+    }
+
+    arg->input_files[arg->input_files_num] = (char *)malloc(strlen(file) + 1);
+    strcpy(arg->input_files[arg->input_files_num], file);
+    arg->input_files_num += 1;
+}
+
+int arguments_parse(arguments_t *arg, int argc, char **argv)
+{
+    int opt;
+    while ((opt = getopt(argc, argv, "io:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            arg->case_sensitive = false;
+            break;
+        case 'o':
+            arg->output_file = strdup(optarg);
+            break;
+        default:
+            return -1;
+        }
+    }
+
+    if (optind < argc)
+    {
+        arg->keyword = strdup(argv[optind]);
+        optind += 1;
+    }
+
+    while (optind < argc)
+    {
+        char *file = strdup(argv[optind]);
+
+        arguments_add_input_file(arg, file);
+
+        optind += 1;
+    }
+
+    return 0;
+}
---
+++ mygrep/src/arguments.h
@@ -0,0 +1,23 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+
+typedef struct arguments
+{
+    char *output_file;
+
+    char *keyword;
+
+    bool case_sensitive;
+
+    char **input_files;
+    int input_files_num;
+    int input_files_capacity;
+} arguments_t;
+
+void arguments_init(arguments_t *arg);
+void arguments_free(arguments_t *arg);
+
+void arguments_add_input_file(arguments_t *arg, char *file);
+int arguments_parse(arguments_t *arg, int argc, char **argv);
+
+#endif
