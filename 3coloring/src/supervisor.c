99de990d597fc83cbad35d3231305bdc3d24670f
LetsMelon <domi.m@outlook.com>
2023-11-20 17:20:28 +0100

    add simple skeleton for the supervisor

--- 3coloring/src/supervisor.c
+++ 3coloring/src/supervisor.c
@@ -1,1 +1,36 @@
-int main(void) { return 0; }
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "circular_buffer.h"
+#include "shared_memory.h"
+
+static char *USAGE = "SYNOPSIS\n\tsupervisor [-n limit] [-w delay] [-p]\n";
+
+typedef struct {
+  int limit, delay;
+
+  bool flag_p;
+} arguments_t;
+
+int main(void) {
+  // ! The struct 'shared_memory_t' must be smaller than 4096 bytes.
+  assert(sizeof(shared_memory_t) <= 4096);
+
+  printf("Hello from the supervisor!\n");
+
+  circular_buffer_t *cb = cb_open_master();
+
+  if (cb == NULL) {
+    printf("Error in creating circular buffer!\n");
+    return EXIT_FAILURE;
+  }
+
+  if (cb_close_master(cb) != 0) {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
