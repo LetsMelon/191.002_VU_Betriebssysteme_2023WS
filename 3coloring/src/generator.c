ad9ccddc9661e92d16f78cc3eeaf30ea98c7a6a7
LetsMelon <domi.m@outlook.com>
2023-11-25 17:59:22 +0100

    working synchronized example

--- 3coloring/src/generator.c
+++ 3coloring/src/generator.c
@@ -1,3 +1,5 @@
+#include <errno.h>
+#include <semaphore.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -7,147 +9,47 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "circular_buffer.h"
+#include "circular_buffer_helper.h"
 #include "edge.h"
 #include "matrix.h"
 #include "parser.h"
+#include "shared_memory.h"
 
 static char *USAGE = "SYNOPSIS\n\tgenerator EDGE1...\nEXAMPLE\n\tgenerator 0-1 "
                      "0-2 0-3 1-2 1-3 2-3\n";
 
-/*
-$ ./generator 0-1 0-5 1-3
-Seed: 216039000
-0-1
-0-0 // ?
-5-0
-3-1
-*/
-
 int main(int argc, char **argv) {
-  struct timespec ts;
-  clock_gettime(CLOCK_MONOTONIC, &ts);
-  long seed = ts.tv_nsec;
-  // Something something random
-  seed = (seed & ((seed >> 8) ^ (seed >> 12))) + getpid();
-  printf("Seed: %ld\n", seed);
-  srandom(seed);
-
-  int input_len = 0;
-  for (int i = 1; i < argc; i += 1) {
-    input_len += strlen(argv[i]);
-    if (i + 1 < argc) {
-      input_len += 1;
-    }
-  }
-
-  char *input_concat = (char *)malloc(sizeof(char) * (input_len + 1));
-  if (input_concat == NULL) {
+  shared_memory_t shared_memory;
+  if (sm_open(&shared_memory, false) < 0) {
     return EXIT_FAILURE;
   }
 
-  int already_copied = 0;
-  for (int i = 1; i < argc; i += 1) {
-    char *argv_i = argv[i];
+  printf("Wait...!\n");
+  sem_wait(shared_memory.semaphore_buffer_mutex);
 
-    strcpy(input_concat + already_copied, argv_i);
-    already_copied += strlen(argv_i);
+  printf("Lets go!\n");
 
-    input_concat[already_copied] = ' ';
-    already_copied += 1;
+  int len = 2;
+  edge_t *edges = malloc(sizeof(edge_t) * len);
+  if (edges != NULL) {
+
+    edges[0].node1 = 0;
+    edges[0].node2 = 2;
+
+    edges[1].node1 = 1;
+    edges[1].node2 = 2;
+
+    cbh_write_edges(shared_memory.buffer, edges, len);
+
+    free(edges);
   }
 
-  input_concat[input_len] = '\0';
+  printf("Wrote everything to the shared buffer!\n");
 
-  string_list_t edges;
+  sem_post(shared_memory.semaphore_buffer_mutex);
 
-  p_split_at(input_concat, ' ', &edges);
-
-  free(input_concat);
-
-  if (edges.num < 1) {
-    sl_free(&edges);
-
-    printf("%s", USAGE);
-
-    return EXIT_FAILURE;
-  }
-
-  // sl_print(&edges);
-
-  edge_t *parsed_edges = (edge_t *)malloc(sizeof(edge_t) * edges.num);
-  if (parsed_edges == NULL) {
-    sl_free(&edges);
-
-    return EXIT_FAILURE;
-  }
-
-  for (size_t i = 0; i < edges.num; i += 1) {
-    edge_t edge;
-    if (p_parse_as_edge(edges.values[i], &edge) != 0) {
-      sl_free(&edges);
-      free(parsed_edges);
-
-      return EXIT_FAILURE;
-    }
-
-    parsed_edges[i] = edge;
-  }
-
-  int n = edges.num;
-  sl_free(&edges);
-
-  graph_t graph;
-  m_graph_init(&graph, parsed_edges, n);
-
-  free(parsed_edges);
-
-  m_am_print(&graph.edges);
-
-  // for (int i = 0; i < graph.nodes_count; i += 1) {
-  //   nodes_t node = graph.nodes[i];
-  //
-  //   printf("node {id: %d, color: 0x%lx}\n", node.id, node.color);
-  // }
-
-  int current_start_index = 0;
-  while (m_graph_is_3colorable(&graph) == false &&
-         current_start_index < graph.nodes_count) {
-
-    edge_t *same_color_neighbors;
-    int same_color_neighbors_count = m_graph_get_same_color_edges(
-        &graph, graph.nodes[current_start_index].id, &same_color_neighbors);
-
-    if (same_color_neighbors_count == 0) {
-      free(same_color_neighbors);
-
-      current_start_index += 1;
-
-      continue;
-    }
-
-    if (same_color_neighbors_count == -1) {
-      m_graph_free(&graph);
-      return EXIT_FAILURE;
-    }
-
-    for (int i = 0; i < same_color_neighbors_count; i += 1) {
-      edge_t e = same_color_neighbors[i];
-
-      printf("DEBUG: %d->%d\n", e.node1, e.node2);
-      // TODO this check should not be necessary, I _think_ it only happens if
-      // every edge has to be deleted
-      if (e.node1 != e.node2) {
-        printf("%d-%d\n", e.node1, e.node2);
-        m_graph_remove_edge(&graph, &e);
-      }
-    }
-
-    free(same_color_neighbors);
-  }
-
-  m_am_print(&graph.edges);
-
-  m_graph_free(&graph);
+  sm_close(&shared_memory, false);
 
   return EXIT_SUCCESS;
 }
--- 3coloring/src/supervisor.c
+++ 3coloring/src/supervisor.c
@@ -31,55 +31,22 @@ int main(void) {
   // ! The struct 'circular_buffer_t' must be smaller than 4096 bytes.
   assert(sizeof(circular_buffer_t) <= 4096);
 
-  // printf("Hello from the supervisor!\n");
-  //
-  // circular_buffer_t *cb = cb_open_master();
-  //
-  // if (cb == NULL) {
-  //   printf("Error in creating circular buffer!\n");
-  //   return EXIT_FAILURE;
-  // }
-  //
-  // if (cb_close_master(cb) != 0) {
-  //   return EXIT_FAILURE;
-  // }
-
-  // circular_buffer_t buffer;
-  // cb_init(&buffer);
-  //
-  // printf("size = %llu\n", BUFFER_SIZE);
-  //
-  // printf("value = %d\n", cb_read(&buffer));
-  //
-  // {
-  //   int len = 2;
-  //   edge_t *edges = (edge_t *)malloc(sizeof(edge_t) * len);
-  //   edges[0].node1 = 1;
-  //   edges[0].node2 = 2;
-  //   edges[1].node1 = 2;
-  //   edges[1].node2 = 3;
-  //   cbh_write_edges(&buffer, edges, len);
-  //   free(edges);
-  // }
-  //
-  // edge_t *edges;
-  // int len = cbh_read_edges(&buffer, &edges);
-  // for (int i = 0; i < len; i += 1) {
-  //   printf("%d->%d\n", edges[i].node1, edges[i].node2);
-  // }
-  // free(edges);
-  //
-  // cb_free(&buffer);
-
   if (sm_open(&shared_memory, true) < 0) {
     return EXIT_FAILURE;
   };
 
   signal(SIGINT, handle_shutdown);
 
+  sem_post(shared_memory.semaphore_buffer_mutex);
+
   while (!in_shutdown) {
+    sem_wait(shared_memory.semaphore_buffer_mutex);
+
+    printf("count: %llu\n", shared_memory.buffer->count);
+
+    sem_post(shared_memory.semaphore_buffer_mutex);
     /* code */
-    sleep(1);
+    sleep(5);
   }
 
   printf("Shutdown ...\n");
