056a5021ef3387f8cf7575940e2acb02b761d6c7
LetsMelon <domi.m@outlook.com>
2023-11-27 11:59:20 +0100

    write output to stdout

--- 3coloring/tests/simple.js
+++ 3coloring/tests/simple.js
@@ -1,10 +1,11 @@
-const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
 
 const main = async () => {
-  let promises = [exec('./supervisor'), exec('./generator 0-1 0-2 1-2 0-3')];
+  let promises = [exec("./supervisor"), exec("./generator 0-1 0-2 1-2 0-3")];
 
-  await Promise.all(promises);
-}
+  const output = await Promise.all(promises);
+  console.log(output);
+};
 
 main();
