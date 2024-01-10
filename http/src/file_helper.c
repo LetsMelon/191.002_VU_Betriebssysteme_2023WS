866bd4c8a541b02de2617946b2f90e0343e7a89f
LetsMelon <domi.m@outlook.com>
2024-01-11 00:23:54 +0100

    add docs to file_helper

--- http/src/file_helper.c
+++ http/src/file_helper.c
@@ -1,3 +1,11 @@
+/**
+ * @file file_helper.c
+ * @author Domenic Melcher <e12220857@student.tuwien.ac.at>
+ * @date 09.01.2024
+ *
+ * @brief Contains implementation for file helper functions.
+ */
+
 #include "file_helper.h"
 
 #include <stdbool.h>
@@ -22,17 +30,16 @@ int combine_file_paths(const char *path1, const char *path2, char **out) {
 
   int char_to_add;
   if (path1[p1_len - 1] == '/' || path2[0] == '/') {
-    char_to_add = 0;
+    char_to_add = 0; // No extra '/' needed
   } else {
-    char_to_add = 1;
+    char_to_add = 1; // Need to add '/'
   }
 
   int n = p1_len + p2_len + char_to_add + 1;
-  char *tmp_out = malloc(sizeof(char) * n);
+  char *tmp_out = calloc(n, sizeof(char));
   if (tmp_out == NULL) {
     return -1;
   }
-  tmp_out[n - 1] = '\0';
 
   if (strcpy(tmp_out, path1) == NULL) {
     free(tmp_out);
@@ -41,7 +48,7 @@ int combine_file_paths(const char *path1, const char *path2, char **out) {
 
   int write_head = p1_len;
   if (char_to_add == 1) {
-    tmp_out[write_head] = '/';
+    tmp_out[write_head] = '/'; // Add '/' between paths
     write_head += 1;
   }
 
--- http/src/file_helper.h
+++ http/src/file_helper.h
@@ -1,3 +1,11 @@
+/**
+ * @file file_helper.h
+ * @author Domenic Melcher <e12220857@student.tuwien.ac.at>
+ * @date 09.01.2024
+ *
+ * @brief Contains file helper functions.
+ */
+
 #ifndef _FH
 #define _FH
 
@@ -5,10 +13,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * @brief Checks if a file exists at the specified path.
+ * @param path The path of the file to check.
+ * @return Returns true if the file exists, otherwise false.
+ */
 bool file_at_path_exists(const char *path);
 
+/**
+ * @brief Gets the size of the file.
+ * @param file Pointer to the file for which to get the size.
+ * @return Returns the size of the file in bytes, or -1 if the size cannot be
+ * determined.
+ */
 long file_size(FILE *file);
 
+/**
+ * @brief Combines two file paths into a single path.
+ * @param path1 The first part of the path.
+ * @param path2 The second part of the path.
+ * @param out Pointer to a character pointer that will hold the combined
+ * path. Memory will be allocated for the combined path and should be freed by
+ * the caller.
+ * @return Returns 0 on success, or an error code if combining the paths fails.
+ */
 int combine_file_paths(const char *path1, const char *path2, char **out);
 
 #endif
